69e468fc4b5d75067cffc2b4cdc3620374188a7c
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2025-04-23 10:19:54 +0000

    update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,12 @@
-let score = "33"
+let score = "33";
 
 console.log(typeof score);
-console.log(typeof (score));
+console.log(typeof score);
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score);
 
 console.log(valueInNumber);
 console.log(typeof valueInNumber);
-
 
 // "33" => 33
 // "33abc" => NaN
@@ -16,10 +15,9 @@ console.log(typeof valueInNumber);
 // undefined => NaN
 // " " => 0
 
+let isLoggedIn = "nishant";
 
-let isLoggedIn = "nishant"
-
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 console.log(booleanIsLoggedIn);
 console.log(typeof booleanIsLoggedIn);
@@ -31,10 +29,9 @@ console.log(typeof booleanIsLoggedIn);
 // null => false
 // undefined => false
 
+let someNumber = " ";
 
-let someNumber = " "
-
-let stringNumber = String(someNumber)
+let stringNumber = String(someNumber);
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
@@ -43,3 +40,50 @@ console.log(typeof stringNumber);
 // "55" => string
 // null => string
 // undefined => string
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++ operations +++++++++++++++++++++++++++++++++++++++++++++++++
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log( 2+2); // add
+// console.log( 2-2); // subs
+// console.log( 2*2); // multy
+// console.log( 2**2); // square
+// console.log( 2/2);  // divide
+// console.log( 2%2); // remainder (modulas)
+
+let str1 = "Hello";
+let str2 = " Nishant";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+// console.log((3+4)*5%3);
+
+console.log(+true);   // 1
+console.log(+""); // 0
+ 
+
+
+ let num1 , num2 , num3;
+
+ num1 = num2 = num3 = 2 + 2
+ 
+ console.log(num2);
+ 
+
+ let gameCounter = 100;
+ ++gameCounter;
+
+ console.log(gameCounter);
+
+ // not suggest to do this type of messy conversion Must avoid
