5ee3bcfdf53370414193d2eaad2ac969a3839adb
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2025-05-03 06:45:13 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,11 +4,12 @@
 // console.log(2==1);
 // console.log(2!=1);
 
+console.log(false > 1);
 console.log("2" > 1);
 console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
+console.log(null > 0);  // null covert as 0
+console.log(null == 0);   //NaN
 console.log(null >= 0);
 
 console.log(undefined == 0);
