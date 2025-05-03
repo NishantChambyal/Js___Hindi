aacccd3ccd7b443f17f0f3b65cf4e1541022dc5e
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2025-05-03 05:04:16 +0000

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,30 +1,33 @@
-"use strict" // Treat all js code as newer version
+"use strict"; //  Treat all js code as newer version
 
-// alert(3+3) // We are using nodejs , not browser
+// alert(3+3) //  We are using node js not browser
 
 console.log(3
-    +3); // code readability should be high
+    +3)                      // do not write the code in that way code readability should be high
+ console.log("nishant");
 
-console.log("nishant");
+let name = "Nishant"
+let age = 18
+let isLoggedIn = false
 
-
-let name = "nishant"
-let age = 25
-let isLoggedIn = true
-let state;
-
-
- // number => 2 to power of 53
- // bigint 
- // String => "" , ''
- // Boolean => true /  false
- // null => stand alone value
- // undefine => 
- // symbol => unique
+// Number  =>  2 to the power of 53
+// bigint  =>  1556213154524154141n
+// string  =>  ""
+// boolean =>  true /  false
+// null    =>  Standalone value (object type)
+// undefined => 
+// symbol  =>  unique
 
 
- // object 
 
- console.log(typeof undefined); // udefined
- console.log(typeof null); // object
- 
+// object 
+
+console.log(typeof "string"); // string
+
+console.log(typeof age);    // number
+
+console.log(typeof null);   // object
+
+console.log(typeof undefined);  // undefined
+
+
